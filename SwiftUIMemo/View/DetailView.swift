04c9bc76d2c2d106faaa7bb0fef3fbf57644ba7c
hooneun <kimhoon0316@gmail.com>
2023-12-29 18:34:27 +0900

    upate

--- SwiftUIMemo/View/DetailView.swift
+++ SwiftUIMemo/View/DetailView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct DetailView: View {
+    @ObservedObject var memo: Memo
+    
+    @EnvironmentObject var store: MemoStore
+    
+    @State private var showComposer = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(memo.content)
+                            .padding()
+                        Spacer(minLength: 0)
+                    }
+                    
+                    Text(memo.insertDate, style: .date)
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("메모 보기")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showComposer) {
+            ComposeView(memo: memo)
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    NavigationView{
+        DetailView(memo: Memo(content: "Hello"))
+            .environmentObject(MemoStore())
+    }
+    
 }
