690edf2396475e0a83b0989720fd0e561c812055
hooneun <kimhoon0316@gmail.com>
2023-12-29 13:27:29 +0900

    MemoList

--- SwiftUIMemo/MainListView.swift
+++ SwiftUIMemo/MainListView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                MemoCell(memo: memo)
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 #Preview {
     MainListView()
+        .environmentObject(
+            MemoStore()
+        )
 }
+
